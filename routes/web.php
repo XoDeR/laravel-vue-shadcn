480cfa48024e76c8f5fe218fee15c18ee0fdf97b
XoDeR <xoder.vs@gmail.com>
2025-08-04 14:29:52 +0200

    added users resource route, added redirect to dashboard form home page

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,25 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome');
+// Route::get('/', function () {
+//     return Inertia::render('Welcome');
+// })->name('home');
+
+Route::get('/', function (): RedirectResponse {
+    return redirect('dashboard');
 })->name('home');
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::resource('users', UserController::class);
+});
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
