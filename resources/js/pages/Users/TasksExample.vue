0de0a5ba4030caf7c4ae0e3b5b76dbfdc5875b21
XoDeR <xoder.vs@gmail.com>
2025-08-04 17:26:45 +0200

    example of datatable from unovue/shadcn-vue (to be adapted)

---
+++ resources/js/pages/Users/TasksExample.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { columns } from '@/components/DataTable/columns'
+import DataTable from '@/components/DataTable/DataTable.vue'
+import tasks from '@/lib/ExampleData/tasks.json'
+</script>
+
+<template>
+  <div class="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+    <DataTable :data="tasks" :columns="columns" />
+  </div>
+</template>
