8b2b7abf5332a40a3d023ebec63bd1555f92736c
XoDeR <xoder.vs@gmail.com>
2025-08-04 14:14:48 +0200

    db seeder -- default admin + generated users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'user']);
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password')
+        ])->assignRole('admin');
+
+        User::factory(50)->create()->each(function ($user) {
+            $user->assignRole('user');
+            $user->created_at = fake()->dateTimeBetween('-1 year', 'now');
+            $user->save();
+        });
     }
 }
